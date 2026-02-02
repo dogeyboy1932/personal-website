457d6b46136ba9c9f17a69724adf215af6fcfaa9
dogeyboy1932 <vagogineni@gmail.com>
2026-02-02 11:10:08 -0600

    Increased font size

--- src/components/Headers/SectionHeader.svelte
+++ src/components/Headers/SectionHeader.svelte
@@ -7,8 +7,8 @@
 </script>
 
 <div class="py-2 mt-4 mb-2">
-  <h2 id={id} class={`uppercase tracking-[0.15em] text-lg font-semibold ${customColor || $theme.sectionHeader} inline-flex items-center gap-3`}>
+  <h1 id={id} class={`uppercase tracking-[0.15em] text-2xl font-semibold ${customColor || $theme.sectionHeader} inline-flex items-center gap-3`}>
     <span class="w-8 h-[2px] {$theme.gradient.sectionLine} rounded-full"></span>
     <span in:fly={{ y: -8, duration: 350 }}>{title}</span>
-  </h2>
+  </h1>
 </div>
